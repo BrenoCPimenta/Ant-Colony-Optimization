7daa0e6ae240768fac6febf469890d2ff44d0608
BrenoCPimenta <brenocpimenta@gmail.com>
2021-03-16 18:49:11 -0300

    Update Ant and Graph classes

--- code/ant.py
+++ code/ant.py
@@ -8,13 +8,13 @@ class Ant():
     by specific Graph.
     The decisions made by the individual
     are probabilistics using a function
-    with the edges weights of the Graph.
+    with the edges attributes of the Graph.
     """
-    def __init__(self, Enviroment, ALPHA, BETA, seed):
+    def __init__(self, Graph, ALPHA, BETA, seed):
         self.seed = seed
         self.ALPHA = ALPHA
         self.BETA = BETA
-        self.G, node_names = Enviroment.getEnviroment()
+        self.G, node_names = Graph.getGraph()
         self.not_visited = node_names
         self.current_node = (-1,-1)
         self.ant_path = []
@@ -32,7 +32,9 @@ class Ant():
         np.random.seed(self.seed)
         #Start the walk on graph proccess
         while self.not_visited:
+            print("FROM: ", self.current_node)
             next_node = self.__chooseNextNode()
+            print("TO: ", next_node)
             self.ant_path.append((self.current_node, next_node))
             self.current_node = next_node
             self.not_visited.remove(next_node)
@@ -49,11 +51,28 @@ class Ant():
             next_node
         """
         node_probabilities = self.__calculateNodeProbabilityChoices()
-        next_node = np.random.choice(
-                        list(node_probabilities.keys()), 
-                        p=list(node_probabilities.values()))
-        return next_node
+        nodes = list(node_probabilities.keys()) 
+        normalized_probabilities = self.__normalizeProbabilities(node_probabilities.values())
+        indexs = [ i for i in range(len(normalized_probabilities))]
+        next_node_index = np.random.choice(
+                                indexs, 
+                                p=normalized_probabilities)
+        return nodes[next_node_index]
         
+    def __normalizeProbabilities(self, node_probabilities):
+        #round values
+        round_probabilities = []
+        for prob in node_probabilities:
+            round_probabilities.append(round(prob, 4))
+        probabilities_sum = sum(round_probabilities)
+        normalized_probabilities = []
+        for prob in round_probabilities:
+            normalized_probabilities.append(prob / probabilities_sum)
+        full_probabilities = sum(normalized_probabilities)
+        while(full_probabilities != 1):
+            normalized_probabilities[-1] -= full_probabilities - 1 
+            full_probabilities = sum(normalized_probabilities)
+        return normalized_probabilities
 
 
     def __calculateNodeProbabilityChoices(self):
@@ -73,7 +92,8 @@ class Ant():
     
         #Calculates the partial probability to every not followed node edge
         for node in self.not_visited:
-            desirability, pheromone = self.G[self.current_node][node]['weight']
+            desirability = self.G[self.current_node][node]['desirability']
+            pheromone = self.G[self.current_node][node]['pheromone']
             partial_probability = (pheromone ** self.ALPHA) * (desirability ** self.BETA)
             node_partial_probabilities.update({node : partial_probability})
             probability_sum += partial_probability
--- code/graph.py
+++ code/graph.py
@@ -11,6 +11,114 @@ class Graph():
         self.G, self.node_names = self.__buildGraph(self.data, init_pheromone)
 
 
+    def __readData(self, file_name):
+        """
+        Reads file and create a first data structure.
+        Structure is a Matrix:
+            Line:Jobs 
+            Column: Machines
+            Content:time of execution.
+        returns:
+            the matrix, the number of jobs and 
+            number of machines.
+        """        
+        jobs = []
+        with open("../test_instances/" + file_name,'r') as file: 
+            for line in file: 
+                machines = {}
+                this_machine = None
+                for j,value in enumerate(line.split()):
+                    if j%2 != 0:
+                        machines.update({this_machine : value})
+                    else:
+                        this_machine = value
+                jobs.append(machines)
+
+        return jobs #Named as data outside this method
+
+
+    def __buildGraph(self, data, init_pheromone):
+        """
+        From the matrix returned on __readData() method
+        creates a Direct, Weighted and FullConnected Graph 
+        using networkx library.
+        This library will facilitate working with edges.
+        Will be added a virtual inital node (-1,-1) that connects
+        to all others nodes.
+
+        returns:
+            Graph and all node names.
+        """
+        num_jobs = len(data)
+        num_machines = len(data[0])
+        num_nodes = num_jobs*num_machines + 1 #All machineXjobs pair plus the initial virtual node
+
+        #Create directed and fully connected graph, with weight zero in every edge:
+        unnamed_graph = nx.complete_graph(num_nodes,  nx.DiGraph())
+
+        #Giving proper names to the nodes:
+        node_names = [(-1,-1)] #Add initial virtual node 
+        for job in range(num_jobs):
+            for machine in range(num_machines):
+                node_names.append((job, machine))
+        mapping = {i:nodename for i,nodename in enumerate(node_names)} #mapping: {0:(-1,-1), 1:(0,0), ...}
+        G = nx.relabel_nodes(unnamed_graph,mapping)
+        
+        #Remove Edges that point to the  initial vitual node, 
+        # so it only remains from initial to other nodes and not the other way arround:
+        for job in range(num_jobs):
+            for machine in range(num_machines):
+                G.remove_edge((job, machine), (-1,-1))
+
+        #Update Pheromone and desirability:
+        for from_job in range(num_jobs):
+            for from_machine in range(num_machines):
+                
+                #Add edge attributes (desirability/pheromone) for edges from virtual node
+                execution_time = data[from_job][str(from_machine)]
+                desirability = 1/int(execution_time)
+                G[(-1,-1)][(from_job,from_machine)]['pheromone'] = init_pheromone
+                G[(-1,-1)][(from_job,from_machine)]['desirability'] = desirability
+
+                for to_job in range(num_jobs):
+                    for to_machine in range(num_machines):
+                        #Add edge attributes (desirability/pheromone) for edges from operational node
+                        if from_job == to_job and from_machine == to_machine:
+                            pass
+                        else:
+                            execution_time = data[to_job][str(to_machine)]
+                            desirability = 1/int(execution_time)
+                            G[(from_job,from_machine)][(to_job,to_machine)]['pheromone'] = init_pheromone
+                            G[(from_job,from_machine)][(to_job,to_machine)]['desirability'] = desirability
+
+        node_names.remove((-1,-1)) #Remove initial virtual node from names
+        return G, node_names
+
+
+    def getGraph(self):
+        """
+        Returns Graph with updated
+        pheromones and node names
+        """
+        return self.G, self.node_names
+
+    #def updatePheromone(self, walk):
+    # desirability
+    #    time = self.__calculateTimeFromWalk(walk)
+
+    #def __calculateTimeFromWalk(self, walk):
+    #    pass
+
+
+
+
+
+
+
+
+
+
+
     def printGraph(self):
         """
         Creates an image of the built graph
@@ -43,110 +151,3 @@ class Graph():
         nx.draw_networkx(self.G, pos, arrows=True, ax=ax, **options)
         fig.savefig('graph.png')
 
-    def __readData(self, file_name):
-        """
-        Reads file and create a first data structure.
-        Structure is a Matrix:
-            Line:Jobs 
-            Column: Machines
-            Content:time of execution.
-        returns:
-            the matrix, the number of jobs and 
-            number of machines.
-        """        
-        jobs = []
-        with open("../test_instances/" + file_name,'r') as file: 
-            for line in file: 
-                machines = {}
-                this_machine = None
-                for j,value in enumerate(line.split()):
-                    if j%2 != 0:
-                        machines.update({this_machine : value})
-                    else:
-                        this_machine = value
-                jobs.append(machines)
-
-        '''
-        for i,job in enumerate(jobs):
-            print("JOB ", i)
-            for key, value in job.items():
-                print("   machine ", key, "  time: ", value)
-        print("\n\n", jobs[0][str(2)])
-        raise "Hi"
-        '''
-        return jobs #Named as data outside method
-
-
-    def __buildGraph(self, data, init_pheromone):
-        """
-        From the matrix returned on __readData() method
-        creates a Direct, Weighted and FullConnected Graph 
-        using networkx library.
-        This library will facilitate working with edges.
-        Will be added a virtual inital node (-1,-1) that connects
-        to all others nodes.
-
-        returns:
-            Graph and all node names.
-        """
-        num_jobs = len(data)
-        num_machines = len(data[0])
-        num_nodes = num_jobs*num_machines + 1 #All machineXjobs pair plus the initial virtual node
-
-        #Create directed and fully connected graph, with weight zero in every edge:
-        unnamed_graph = nx.complete_graph(num_nodes,  nx.DiGraph(weight=0))
-
-        #Giving proper names to the nodes:
-        node_names = [(-1,-1)] #Add initial virtual node 
-        for job in range(num_jobs):
-            for machine in range(num_machines):
-                node_names.append((job, machine))
-        mapping = {i:nodename for i,nodename in enumerate(node_names)} #mapping: {0:(-1,-1), 1:(0,0), ...}
-        G = nx.relabel_nodes(unnamed_graph,mapping)
-        
-        #Remove Edges that point to the  initial vitual node, 
-        # so it only remains from initial to other nodes and not the other way arround:
-        for job in range(num_jobs):
-            for machine in range(num_machines):
-                G.remove_edge((job, machine), (-1,-1))
-
-        #Update Pheromone and desirability:
-        for from_job in range(num_jobs):
-            for from_machine in range(num_machines):
-                
-                #Add weight (desirability/pheromone) for edges from virtual node
-                execution_time = data[from_job][str(from_machine)]
-                desirability = 1/int(execution_time)
-                #G[(-1,-1)][(from_job,from_machine)]['weight'] = (desirability, init_pheromone)
-                G[(-1,-1)][(from_job,from_machine)]['pheromone'] = init_pheromone
-                G[(-1,-1)][(from_job,from_machine)]['desirability'] = desirability
-
-                for to_job in range(num_jobs):
-                    for to_machine in range(num_machines):
-                        #Add weight (desirability/pheromone) for edges from operational node
-                        if from_job == to_job and from_machine == to_machine:
-                            pass
-                        else:
-                            execution_time = data[to_job][str(to_machine)]
-                            desirability = 1/int(execution_time)
-                            #G[(from_job,from_machine)][(to_job,to_machine)]['weight'] = (desirability, init_pheromone)
-                            G[(from_job,from_machine)][(to_job,to_machine)]['pheromone'] = init_pheromone
-                            G[(from_job,from_machine)][(to_job,to_machine)]['desirability'] = desirability
-
-        node_names.remove((-1,-1)) #Remove initial virtual node from names
-        return G, node_names
-
-
-    def getGraph(self):
-        """
-        Returns Graph with updated
-        pheromones and node names
-        """
-        return self.G, self.node_names
-
-    #def updatePheromone(self, walk):
-    # desirability
-    #    time = self.__calculateTimeFromWalk(walk)
-
-    #def __calculateTimeFromWalk(self, walk):
-    #    pass
